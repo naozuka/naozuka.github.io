8425df6cfa0423525b68fd9d4eb6d04e765ba732
Mauricio Mitsuharu Naozuka <naozuka@gmail.com>
2023-06-13 18:16:43 -0300

    Changed to data.js to be smaller

--- test/cdh-filter-levels/index.js
+++ test/cdh-filter-levels/index.js
@@ -1,59 +1,16 @@
-let campaigns = []
 let monstersList = []
-let monsterName = ""
-let opNormal = false
-let opHard = false
-let opCrazy = false
-let opNightmare = false
-let firstInput = 0
-let searchTitle = ""
 
-function addMonster(arr) {	
-	for (let i = 0; i < arr.length; i++) {		
-		if (!monstersList.find(x => x === arr[i].name)) {
-			monstersList.push(arr[i].name);
-		}
-	}
-}
-
-function buildCampaigns() {
-	let data = getData()
-	
-	for (let i = 0; i < data.length; i++) {
-		for (let j = 0; j < data[i].levels.length; j++) {			
-			campaigns.push({
-				campaign: data[i].campaign,
-				levelname: data[i].levels[j].levelname.trim()+" ["+data[i].levels[j].difficulty+"-"+data[i].levels[j].levelnumber+"]",
-				difficulty: data[i].levels[j].difficulty,
-				energy: data[i].levels[j].energy,
-				totalmana: data[i].levels[j].totalmana,
-				monsters: data[i].levels[j].monsters,
-				factions: data[i].levels[j].factions
-			})
-			
-			addMonster(data[i].levels[j].monsters)
-			addMonster(data[i].levels[j].factions)			
-		}
-	}
-}
-
-function findInArray(arr) {
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i].name === monsterName) {
-			return arr[i].total
-		}
-	}
-	return 0
-}
-
-function selectAmount(el) {	
-    let amount = findInArray(el.monsters)
-	
-	if (amount == 0) 
-		amount = findInArray(el.factions)
-	
-	return amount
-}
+// Structure
+const CAMPAIGN_POS = 0
+const MONSTERS_POS = 1
+const FACTIONS_POS = 2
+const ALL_LEVELS_POS = 3
+	const ENERGY_POS = 0
+	const DIFFICULTY_POS = 1
+	const LEVEL_POS = 2
+		const TOTAL_MANA_POS = 0
+		const MONSTERS_COUNT_POS = 1
+		const FACTIONS_COUNT_POS = 2
 
 function sortDescAmount(arr) {
 	arr.sort(function(a, b) {
@@ -68,51 +25,13 @@ function sortDescAmount(arr) {
 	return arr
 }
 
-function isSelected(difficulty) {
-	if (difficulty == "NO" && opNormal)
-		return true
-	
-	if (difficulty == "HA" && opHard)
-		return true
-	
-	if (difficulty == "CR" && opCrazy)
-		return true
-	
-	if (difficulty == "NM" && opNightmare)
-		return true
-	
-	return false
-}
-
-function filterMonster() {
-    let monsters = campaigns.map(function(el, i) {
-        return { 			
-			campaign: el.campaign,
-			levelname: el.levelname,
-			difficulty: el.difficulty, 
-			energy: el.energy,
-			totalmana: el.totalmana,
-			amount: selectAmount(el)
-		}
-    })
-
-    monsters = monsters.filter(x => x.amount > 0 && isSelected(x.difficulty))
-	monsters = sortDescAmount(monsters)
-	
-	if (firstInput > 0) {
-		monsters = monsters.slice(0, firstInput)
-	}
-
-    return monsters
-}
-
 function injectHtml(idselector, html) {
 	let el = document.querySelector(idselector)
     el.innerHTML = "";
     el.insertAdjacentHTML('afterbegin', html)
 }
 
-function insertResult(arrayData) {
+function insertResult(arrayData, searchTitle) {
 	if (arrayData.length == 0) {
 		return
 	}
@@ -124,7 +43,7 @@ function insertResult(arrayData) {
         "    <th>Campaign - Level</th>"+
 		"    <th>Energy</th>"
 		
-	if (arrayData[0].totalmana != undefined) {
+	if (arrayData[0].totalMana != undefined) {
 		result += "<th>Total Mana</th>"
 	}
 	
@@ -136,11 +55,11 @@ function insertResult(arrayData) {
 
     for (i = 0; i < arrayData.length; i++) {
         result += "<tr>"+
-            "<td>"+arrayData[i].campaign+" - "+arrayData[i].levelname+"</td>"+
+            "<td>"+arrayData[i].campaign+" - "+arrayData[i].levelName+"</td>"+
 			"<td>"+arrayData[i].energy+"</td>"
 			
-		if (arrayData[0].totalmana != undefined) {
-			result += "<td>"+arrayData[i].totalmana+"</td>"
+		if (arrayData[0].totalMana != undefined) {
+			result += "<td>"+arrayData[i].totalMana+"</td>"
 		}
 		
 		result += "<td>"+arrayData[i].amount+"</td>"
@@ -154,9 +73,16 @@ function insertResult(arrayData) {
 	injectHtml("#span-result", result)
 }
 
-function buildMonstersList() {
-	let options = ""
+function buildMonstersList() {	
+	for (let i = 0; i < MONSTERS.length; i++) {
+		monstersList.push(MONSTERS[i])
+	}
 	
+	for (let i = 0; i < FACTIONS.length; i++) {
+		monstersList.push(FACTIONS[i])
+	}
+	
+	let options = ""	
 	for (let i = 0; i < monstersList.length; i++) {
 		options += "<option value='"+monstersList[i]+"'>"
 	}
@@ -164,52 +90,137 @@ function buildMonstersList() {
 	injectHtml("#monsters-list", options)
 }
 
-function setParameters() {
-	monsterName = document.getElementById("input-monster").value
-	opNormal = document.getElementById("input-normal").checked
-	opHard = document.getElementById("input-hard").checked
-	opCrazy = document.getElementById("input-crazy").checked
-	opNightmare = document.getElementById("input-nightmare").checked
-	firstInput = document.getElementById("input-first").value
+function getParameters() {
+	const searchValue = document.getElementById("input-monster").value
+	
+	let isMonster = false
+	let searchId = FACTIONS.indexOf(searchValue)	
+	
+	if (searchId == -1) {
+		searchId = MONSTERS.indexOf(searchValue)
+		isMonster = true
+	}
+	
+	return {
+		searchValue: searchValue,
+		searchId: searchId,
+		isMonster: isMonster,
+		top: parseInt(document.getElementById("input-first").value),
+		listTotalMana: false,
+		difficulties: {
+			normal: document.getElementById("input-normal").checked,
+			hard: document.getElementById("input-hard").checked,
+			crazy: document.getElementById("input-crazy").checked,
+			nightmare: document.getElementById("input-nightmare").checked
+		}
+	}
 }
 
-function findLevels() {
-    setParameters()
-	searchTitle = document.getElementById("input-monster").value
+function isDifficultySelected(difficulty, difficulties) {
+	if ((difficulty == 0 && difficulties.normal) || 
+	    (difficulty == 1 && difficulties.hard) ||
+	    (difficulty == 2 && difficulties.crazy) ||
+	    (difficulty == 3 && difficulties.nightmare)) {
+		return true
+	}
 	
-	let result = filterMonster()
-    insertResult(result)    
+	return false
+}
+
+function getDifficultyDescription(difficulty) {
+	let description = "";
+	switch (difficulty) {
+		case 0: description = "NO" 
+			break
+		case 1: description = "HA" 
+			break
+		case 2: description = "CR" 
+			break
+		case 3: description = "NM" 
+			break
+	}
+	return description
+}
+
+function filterMonster(parameters) {
+	let filtered = []
+
+	console.log(parameters)
+	
+	for (let campaignIdx = 0; campaignIdx < CAMPAIGNS.length; campaignIdx++) { // campaigns
+		var campaign = CAMPAIGNS[campaignIdx]
+		const indexMonster = (parameters.isMonster ? MONSTERS_POS : FACTIONS_POS)		
+		const searchIndex = campaign[indexMonster].indexOf(parameters.searchId)
+
+		if (!parameters.listTotalMana && searchIndex == -1) {
+			continue
+		}
+		
+		for (let j = 0; j < campaign[ALL_LEVELS_POS].length; j++) { // campaign by difficulty
+			const difficulty = campaign[ALL_LEVELS_POS][j]			
+			
+			if (!isDifficultySelected(difficulty[DIFFICULTY_POS], parameters.difficulties)) {
+				continue
+			}
+
+			console.log(difficulty, campaign[CAMPAIGN_POS])
+
+			for (let levelIdx = 0; levelIdx < difficulty[LEVEL_POS].length; levelIdx++) { // levels			
+				const level = difficulty[LEVEL_POS][levelIdx]
+				
+				let amount = 0
+				if (parameters.listTotalMana) {
+					amount = level[TOTAL_MANA_POS]
+				} else {			
+					const indexToFilter = (parameters.isMonster ? MONSTERS_COUNT_POS : FACTIONS_COUNT_POS)
+					amount = level[indexToFilter][searchIndex]
+				}
+				
+				filtered.push({
+					campaign: campaign[CAMPAIGN_POS],
+					levelName: LEVELS[campaignIdx][levelIdx]+" ["+
+						getDifficultyDescription(difficulty[DIFFICULTY_POS])+"-"+
+						(levelIdx+1).toString()+"]",
+					energy: difficulty[ENERGY_POS],
+					totalMana: level[TOTAL_MANA_POS],
+					amount: amount
+				})
+			}
+		}		
+	}
+	
+	filtered = sortDescAmount(filtered)
+	filtered = filtered.slice(0, parameters.top)
+
+    return filtered
+}
+
+function findLevels() {    
+	const parameters = getParameters()	
+	const searchTitle = document.getElementById("input-monster").value
+	
+	const result = filterMonster(parameters)
+    insertResult(result, searchTitle)
 }
 
 function listTotalMana() {
-	setParameters()
-	searchTitle = "Total Mana"
+	let parameters = getParameters()
+	parameters.listTotalMana = true
+	const searchTitle = "Total Mana"	
 	
-	let listTotalMana = campaigns.map(function(el, i) {
-        return { 			
-			campaign: el.campaign,
-			levelname: el.levelname,
-			difficulty: el.difficulty,
-			energy: el.energy,
-			amount: el.totalmana
-		}
-    })
-
-    listTotalMana = listTotalMana.filter(x => isSelected(x.difficulty))
-	listTotalMana = sortDescAmount(listTotalMana)
-	
-	if (firstInput > 0) {
-		listTotalMana = listTotalMana.slice(0, firstInput)
-	}
-		
-    insertResult(listTotalMana)
+	const result = filterMonster(parameters)
+    insertResult(result, searchTitle)
 }
 
 function updateMonsterList() {
 	let input = document.getElementById("input-monster")	
 }
 
-window.onload = function() {	
-	buildCampaigns()
+window.onload = function() {
 	buildMonstersList()
+	
+	document.getElementById("input-monster").value = "Armored Orc"	
+	const parameters = getParameters()	
+	const result = filterMonster(parameters)
+	insertResult(result, "Armored Orc")	
 }
